9ae6ce6e5abd3f8a6436027b8bdbc2a03280f5b3
Yifei Chen <yifeichen3@gmail.com>
2021-08-10 09:12:57 -0700

    adding basic styles

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -17,6 +18,7 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
