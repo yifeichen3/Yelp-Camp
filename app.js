b26ef05a51f74bbb0b68dc0df264d8f9b191339d
Yifei Chen <yifeichen3@gmail.com>
2021-09-05 11:24:11 -0700

    Basic Authorization

--- app.js
+++ app.js
@@ -6,9 +6,13 @@ const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const flash = require('connect-flash')
 const session = require('express-session')
-
-const campgrounds = require('./routes/campgrounds');
-const reviews = require('./routes/review');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const userRoutes = require('./routes/users');
+const campgroundRoutes = require('./routes/campgrounds');
+const reviewRoutes = require('./routes/review');
+const User = require('./models/user');
+const {isLoggedIn} = require('./middleware');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -42,18 +46,35 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
 }
+
 app.use(session(sessionConfig))
 app.use(flash())
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
+    console.log(req.session);
+    res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
     next();
 })
 
-app.use('/campgrounds', campgrounds);
-app.use('/campgrounds/:id/reviews', reviews);
+app.use('/', userRoutes);
+app.use('/campgrounds', campgroundRoutes);
+app.use('/campgrounds/:id/reviews', reviewRoutes);
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/fake', async(req, res) => {
+    const user = new User({email: 'yifei@gmail.com', username: 'yifei'});
+    const newUser = await User.register(user, 'chicken');
+    res.send(newUser);
+})
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -65,10 +86,10 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
-    if (err) {
-        req.flash('error', 'Something went wrong');
-        return res.redirect('/campgrounds');
-    }
+    // if (err) {
+    //     req.flash('error', 'Something went wrong');
+    //     return res.redirect('/campgrounds');
+    // }
     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
     res.status(statusCode).render('error', { err })
 })
--- seeds/index.js
+++ seeds/index.js
@@ -25,6 +25,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '6133fbe31517ce63a8c19211',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
